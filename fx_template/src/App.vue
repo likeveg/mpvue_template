01afbd3c03021496c5a03000647714238862cf9d
liangbk <503922889@qq.com>
2018-12-17 16:25:10 +0800

    fc

--- fx_template/src/App.vue
+++ fx_template/src/App.vue
@@ -1,31 +1,16 @@
 <script>
+/* 这部分相当于原生小程序的 app.js */
 export default {
   created () {
-    // 调用API从本地缓存中获取数据
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    console.log('app created and cache logs by setStorageSync')
+    console.log('miniapp created!!!')
   }
 }
 </script>
 
 <style>
+/* 这部分相当于原生小程序的 app.wxss */
 .container {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding: 200rpx 0;
-  box-sizing: border-box;
-}
-/* this rule will be remove */
-* {
-  transition: width 2s;
-  -moz-transition: width 2s;
-  -webkit-transition: width 2s;
-  -o-transition: width 2s;
+  background-color: #cccccc;
 }
 </style>
+
--- fx_template/src/main.js
+++ fx_template/src/main.js
@@ -6,3 +6,15 @@ App.mpType = 'app'
 
 const app = new Vue(App)
 app.$mount()
+
+export default {
+  config: {
+    pages: ['^pages/index/main'],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: '第一个小程序',
+      navigationBarTextStyle: 'black'
+    }
+  }
+}
